bcb5b1b76ecaee98fbc19f1cab9f4e5979c6bc75
Kishore S <kishorekish2006@gmail.com>
2025-11-30 22:32:39 +0530

    Add files via upload

---
+++ Week 1/C file/SumNNaturalCubes.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+printf("CH.SC.U4CSE24222\n");
+int n;
+int sum=0;
+printf("Enter value of n:");
+scanf("%d",&n);
+for(int i=1;i<n+1;i++){
+sum+=i*i*i;
+}
+printf("The sum of cubes of first %d natural numbers is %d\n",n,sum);
+}
+
