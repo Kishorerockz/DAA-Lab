2eb0b042848e59353b83927e787238767f998e48
Kishore S <kishorekish2006@gmail.com>
2025-12-27 19:58:59 +0530

    Add files via upload

---
+++ Week 3/C file/dfs.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int visited[20];
+int adj[20][20];
+int n;
+
+void dfs(int v) {
+    int i;
+    visited[v] = 1;
+    printf("%d ", v);
+
+    for (i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && visited[i] == 0) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int i, j, start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++)
+        visited[i] = 0;
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    printf("DFS traversal: ");
+    dfs(start);
+
+    return 0;
+}
+
